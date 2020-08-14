c21574bb9c38ffb3909cd76a1c4491192deb2b88
Elijahscriptdev <elijahobominuru@gmail.com>
2020-08-14 01:08:18 +0100

    set up jest, add test

---
+++ __tests__/Game.test.js
@@ -0,0 +1,24 @@
+// import SubmitScoreScene from '../src/Scenes/SubmitScoreScene'
+import Game from '../src/Objects/Game';
+import config from '../src/config/config';
+
+beforeAll(() => {
+  game = new Game(config);
+});
+
+let game;
+test('Expect score to be 0 when start the game', () => {
+  expect(game.globals.score).toEqual(0);
+});
+
+test('Expect sound to be enable when the game start', () => {
+  expect(game.globals.model.soundOn).toBe(true);
+});
+
+test('Expect sound to be enable when the game start', () => {
+  expect(game.globals.model.soundOn).toBe(true);
+});
+
+test('Expect music to be enable when the game start', () => {
+  expect(game.globals.model.musicOn).toBe(true);
+});
--- src/index.js
+++ src/index.js
@@ -1,30 +1,3 @@
-import phaser from 'phaser';
-import config from './config/config';
-import GameScene from './Scenes/GameScene';
-import BootScene from './Scenes/BootScene';
-import PreloaderScene from './Scenes/PreloaderScene';
-import TitleScene from './Scenes/TitleScene';
-import OptionsScene from './Scenes/OptionsScene';
-import CreditsScene from './Scenes/CreditsScene';
-import SubmitScoreScene from './Scenes/SubmitScoreScene'
-import ScoreScene from './Scenes/ScoreScene'
-import Model from './Model';
-
-class Game extends Phaser.Game {
-  constructor () {
-    super(config);
-    const model = new Model();
-    this.globals = { model, bgMusic: null };
-    this.scene.add('Boot', BootScene);
-    this.scene.add('Preloader', PreloaderScene);
-    this.scene.add('Title', TitleScene);
-    this.scene.add('Options', OptionsScene);
-    this.scene.add('Credits', CreditsScene);
-    this.scene.add('Game', GameScene);
-    this.scene.add('SubmitScore', SubmitScoreScene);
-    this.scene.add('Score', ScoreScene);
-    this.scene.start('Boot');
-  }
-}
+import Game from './Objects/Game';
 
 window.game = new Game();
